3d5a84623b783f0e510b2b35bc2a6441ab65a49f
Jun Matsushita <jun@iilab.org>
2015-01-12 00:18:55 +0100

    Worked on theme

--- theme/assets/js/soon/custom.js
+++ theme/assets/js/soon/custom.js
@@ -1,27 +1,75 @@
-/******************************************************************************************************************************
-WEBSOCKET
-*******************************************************************************************************************************/
-var ws = $.websocket("ws://127.0.0.1:1880/ws/tweets", {
-       	open: function() { console.log(' open')  },
-        close: function() { console.log(' close')  },
-        events: {
-                say: function(e) {
-                        console.log(e.data.name); // 'foo'
-                        console.log(e.data.text); // 'baa'
-                }
-        }
-});
 
 
 /******************************************************************************************************************************
 COMING SOON PAGE
 *******************************************************************************************************************************/
 (function($) {
+
+
+    /******************************************************************************************************************************
+    WEBSOCKET
+    *******************************************************************************************************************************/
+
+    var current_end = new Date();
+
+    var ws = new WebSocket("ws://127.0.0.1:1880/ws/tweets")
+
+    ws.onopen = function (event) {
+//      var now = new Date()
+//      setDate(now)
+      setCount(0,0);
+      setTweets([],0,15)
+      ws.send("connected")
+    };
+    
+
+    ws.onmessage = function(event) { 
+        data = JSON.parse(event.data); 
+        if (data.payload == "connected") return true;
+        console.log(data)
+        if (!data.pour) {
+            current_start = new Date(data.cycle.start);
+            current_end = new Date(data.cycle.end);
+            setDate(current_end);
+            setCount(data.count, data.cycle.threshold)
+            if (data.tweets.length !=0) {
+                setTweets(data.tweets, data.count, data.cycle.tweet_window);
+            }
+        } else if (data.reset) {
+            console.log('reset')
+            var now = new Date()
+            setDate(now)
+            setCount(0,0);
+            setTweets([],0,15)
+        }
+    };
+
+    function setCount(count, threshold) {
+        $("#count").html(count);
+        $("#threshold").html(threshold);
+    }
+
+    function setTweets(tweets, count, tweet_window) {
+        $("#tweets li").each(function(idx, el) {
+            index = count - tweet_window + idx;
+            if (index < 0) {
+                $(el).html('&nbsp;');
+            } else if (count < 15) {
+                var tweet = tweets[index];
+                $(el).css('opacity', (idx+1)/tweet_window);
+                $(el).html(index+1 + '. @' + tweet.user.screen_name + ':' + tweet.text);
+            } else {
+                var tweet = tweets[idx];
+                $(el).css('opacity', (idx+1)/tweet_window);
+                $(el).html(index+1 + '. @' + tweet.user.screen_name + ':' + tweet.text);
+            }
+        });
+    }
+
     /**
     * Set your date here  (YEAR, MONTH (0 for January/11 for December), DAY, HOUR, MINUTE, SECOND)
     * according to the GMT+0 Timezone
     **/
-    var launch = new Date(2015, 06, 14, 11, 00);
     /**
     * The script
     **/
@@ -31,20 +79,20 @@ COMING SOON PAGE
     var minutes = $('#minutes');
     var seconds = $('#seconds');
     
-    setDate();
     function pad(n, width, z) {
             z = z || '0';
             n = n + '';
             return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
             }
-    function setDate(){
+    function setDate(launch){
         var now = new Date();
-        if( launch < now ){
-            days.html('<h1>0</H1><p>Day</p>');
-            hours.html('<h1>0</h1><p>Hour</p>');
-            minutes.html('<h1>0</h1><p>Minute</p>');
-            seconds.html('<h1>0</h1><p>Second</p>');
-            message.html('OUR SITE IS NOT READY YET...');
+        if( launch <= now ){
+            //days.html('<h1>0</H1><p>Day</p>');
+            //hours.html('<h1>0</h1><p>Hour</p>');
+            minutes.html('<h1>00:</h1>');
+            seconds.html('<h1>00</h1>');
+            //message.html('OUR SITE IS NOT READY YET...');
+            ws.send("connected")
         }
         else{
             var s = -now.getTimezoneOffset()*60 + (launch.getTime() - now.getTime())/1000;
@@ -103,52 +151,5 @@ ANIMATIONS
         $(window).scroll(function () {
             animated_contents();
         });
-    });
-})(jQuery);
-/******************************************************************************************************************************
-SLIDER
-*******************************************************************************************************************************/
-(function($) {
-    "use strict";
-    $("body.nomobile #slider").revolution(
-    {
-            delay:9000,
-            startheight:450,
-            startwidth:890,
-
-            thumbWidth:100,
-            thumbHeight:50,
-            thumbAmount:5,
-
-            onHoverStop:"on",
-            hideThumbs:200,
-            navigationType:"bullet",
-            navigationStyle:"round",
-            navigationArrows:"none",
-
-            touchenabled:"on",
-
-            navOffsetHorizontal:0,
-            navOffsetVertical:80,
-            shadow:undefined,
-            fullWidth:"on",
-            fullScreen:"on"
-    });
-})(jQuery);
-/******************************************************************************************************************************
-BOOTSTRAP
-*******************************************************************************************************************************/
-(function($) {
-    "use strict";
-        $('[data-rel=tooltip]').tooltip();
-        $(".alert").alert();
-})(jQuery);
-/******************************************************************************************************************************
-PROGRESS BAR
-*******************************************************************************************************************************/
-(function($) {
-    "use strict";
-    $("a.btn-progress").click(function(){
-        $('#bar-container').slideToggle(); 
     });
 })(jQuery);
